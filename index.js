55b8928c43335d54b039ef8c2cb40423bbf651ff
Erica Schimmel <ericaschimmel01@gmail.com>
2020-01-05 19:58:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHoldaysTo(name) {
+  return `Happy holidays, ${name}`
+}
+
+function happyHolidayTo(name, holiday) {
+  return `Happy, ${holiday}, ${name}!`
+}
